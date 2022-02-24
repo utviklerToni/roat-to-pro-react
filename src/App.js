cd391fd1de14525b3de090ac538d4df1b8046e3b
utviklerToni <h1de.toni@protonmail.com>
2022-02-24 08:26:17 +0100

    studying react by robin-road-to-react, finished props page 43

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+	const stories = [
+		{
+			title: 'React',
+			url: 'https://reactjs.org/',
+			author: 'Jordan Walke',
+			num_comments: 3,
+			points: 4,
+			objectID: 0,
+		},
+		{
+			title: 'Redux',
+			url: 'https://redux.js.org/',
+			author: 'Dan Abramov, Andrew Clark',
+			num_comments: 2,
+			points: 5,
+			objectID: 1,
+		},
+	];
+
+	// callback function
+	const handleSearch = (event) => {
+		console.log(event.target.value);
+	};
+
+	return (
+		<div>
+			<h1>My Hacker Stories</h1>
+
+			<Search onSearch={handleSearch} />
+
+			<hr />
+
+			<List list={stories} />
+		</div>
+	);
+};
+
+const Search = (props) => {
+	const [searchTerm, setSearchTerm] = useState('');
+
+	const handleChangeEvent = (event) => {
+		// console.log(event.target.value);
+		setSearchTerm(event.target.value);
+		props.onSearch(event);
+	};
+
+	return (
+		<div>
+			<label htmlFor='search'>Search: </label>
+			<input id='search' type='text' onChange={handleChangeEvent} />
+
+			<p>{searchTerm}</p>
+		</div>
+	);
+};
+
+const List = (props) =>
+	props.list.map((item) => (
+		<div key={item.objectID}>
+			<span>
+				<a href={item.url}>{item.title}</a>
+			</span>
+			<span>{item.author}</span>
+			<span>-{item.num_comments}</span>
+			<span>-{item.points}</span>
+		</div>
+	));
 
 export default App;
